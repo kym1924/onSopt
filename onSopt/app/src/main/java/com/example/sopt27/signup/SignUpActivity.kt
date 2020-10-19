a059d807db50b94de5aef94cfc9a2bc16eda1982
kym1924 <kym1924@naver.com>
2020-10-19 18:44:39 +0900

    [UPDATE] Password Check

--- onSopt/app/src/main/java/com/example/sopt27/signup/SignUpActivity.kt
+++ onSopt/app/src/main/java/com/example/sopt27/signup/SignUpActivity.kt
@@ -2,13 +2,16 @@ package com.example.sopt27.signup
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.sopt27.R
 import com.example.sopt27.databinding.ActivitySignUpBinding
 import com.example.sopt27.signin.SignInActivity
 import com.example.sopt27.util.showToast
+import com.example.sopt27.util.textChangedListener
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -37,5 +40,17 @@ class SignUpActivity : AppCompatActivity() {
             }
             else showToast("입력하신정보를확인하세요")
         }
+
+        et_pw.textChangedListener{
+            signUpViewModel.checkPassword(et_pw.text.toString(), et_pw_check.text.toString())
+        }
+
+        et_pw_check.textChangedListener {
+            signUpViewModel.checkPassword(et_pw.text.toString(), et_pw_check.text.toString())
+        }
+
+        signUpViewModel.isSamePassword.observe(this, Observer{ value->
+            value.let {img_check_pw.visibility = if(value) View.VISIBLE else View.INVISIBLE }
+        })
     }
 }
--- onSopt/app/src/main/java/com/example/sopt27/signup/SignUpViewModel.kt
+++ onSopt/app/src/main/java/com/example/sopt27/signup/SignUpViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.sopt27.signup
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -8,17 +9,28 @@ class SignUpViewModel : ViewModel() {
     val name = MutableLiveData<String>()
     val id = MutableLiveData<String>()
     val password = MutableLiveData<String>()
+    val passwordCheck = MutableLiveData<String>()
+
+    private val _isSamePassword = MutableLiveData<Boolean>(false)
+    val isSamePassword : LiveData<Boolean>
+        get() = _isSamePassword
+
     val isValid = MutableLiveData<Boolean>()
 
     init{
         name.value = ""
         id.value = ""
         password.value = ""
+        passwordCheck.value = ""
         isValid.value = false
+    }
+
+    fun checkPassword(password : String, passwordCheck : String){
+        _isSamePassword.value = password == passwordCheck
     }
 
     fun validation() {
         isValid.value = false
-        if(!name.value.isNullOrEmpty() && !id.value.isNullOrEmpty() && !password.value.isNullOrEmpty()) isValid.value = true
+        if(!name.value.isNullOrEmpty() && !id.value.isNullOrEmpty() && isSamePassword.value!!) isValid.value = true
     }
 }
