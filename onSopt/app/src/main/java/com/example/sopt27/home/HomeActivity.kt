d8adcd361ee37620cafd2db91eae006e45290f68
kym1924 <kym1924@naver.com>
2020-10-19 17:43:41 +0900

    [UPDATE] Logout

--- onSopt/app/src/main/java/com/example/sopt27/home/HomeActivity.kt
+++ onSopt/app/src/main/java/com/example/sopt27/home/HomeActivity.kt
@@ -1,12 +1,27 @@
 package com.example.sopt27.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sopt27.R
+import com.example.sopt27.signin.SignInActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val sharedPref = getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val sharedEdit = sharedPref.edit()
+
+        btn_logout.setOnClickListener{
+            sharedEdit.clear()
+            sharedEdit.apply()
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
