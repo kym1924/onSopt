d5ba85bdf46cba830a3e103eef19965fc5c5f02b
kym1924 <kym1924@naver.com>
2020-10-22 13:16:17 +0900

    [MODIFY] data class User

--- onSopt/app/src/main/java/com/kimym/onsopt/room/User.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/room/User.kt
@@ -1,13 +1,19 @@
 package com.example.sopt27.room
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.SimpleDateFormat
 
+@Parcelize
 @Entity(tableName = "userDb")
 data class User (
     @PrimaryKey(autoGenerate = true)
     val idx : Int,
     val name : String,
     val id : String,
-    val password : String
-)
+    val password : String,
+    val part : String = "android",
+    val date : String = SimpleDateFormat("yyyy-MM-dd").format(System.currentTimeMillis())
+) : Parcelable
