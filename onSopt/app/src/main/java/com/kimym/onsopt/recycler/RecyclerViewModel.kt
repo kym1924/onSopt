1f442fa2acd502925e8023fa387aa26333354c03
kym1924 <kym1924@naver.com>
2020-10-22 22:24:03 +0900

    [UPDATE] RecyclerViewModel.delete(position)

--- onSopt/app/src/main/java/com/kimym/onsopt/recycler/RecyclerViewModel.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/recycler/RecyclerViewModel.kt
@@ -2,16 +2,23 @@ package com.kimym.onsopt.recycler
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kimym.onsopt.room.User
 import com.kimym.onsopt.room.UserDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RecyclerViewModel : ViewModel(){
 
     lateinit var userDao : UserDao
     lateinit var allUsers : LiveData<List<User>>
 
-    fun init(dao : UserDao){
+    fun init(dao : UserDao) {
         userDao = dao
         allUsers = userDao.getAll()
+    }
+
+    fun delete(position : Int) = viewModelScope.launch(Dispatchers.IO) {
+        userDao.deleteUser(allUsers.value!![position])
     }
 }
--- onSopt/app/src/main/java/com/kimym/onsopt/room/UserDao.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/room/UserDao.kt
@@ -1,10 +1,7 @@
 package com.kimym.onsopt.room
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface UserDao {
@@ -13,4 +10,7 @@ interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUser(user : User)
+
+    @Delete
+    suspend fun deleteUser(user : User)
 }
