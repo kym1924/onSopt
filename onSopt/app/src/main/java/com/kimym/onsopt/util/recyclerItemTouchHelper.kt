571123a0812e58e8df5c24bf67463afff0793dc0
kym1924 <kym1924@naver.com>
2020-10-24 14:29:24 +0900

    [UPDATE] recyclerItemTouchHelper.onMove

--- onSopt/app/src/main/java/com/kimym/onsopt/util/recyclerItemTouchHelper.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/util/recyclerItemTouchHelper.kt
@@ -4,9 +4,11 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.kimym.onsopt.recycler.RecyclerViewModel
+import com.kimym.onsopt.room.User
+import java.util.*
 
 
-fun recyclerItemTouchHelper(recyclerView : RecyclerView, viewModel : RecyclerViewModel) {
+fun recyclerItemTouchHelper(recyclerView : RecyclerView, viewModel : RecyclerViewModel, userList : ArrayList<User>) {
     val itemTouchHelper = ItemTouchHelper(
         object : ItemTouchHelper.SimpleCallback((ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.START or ItemTouchHelper.END), ItemTouchHelper.LEFT) {
 
@@ -19,12 +21,28 @@ fun recyclerItemTouchHelper(recyclerView : RecyclerView, viewModel : RecyclerVie
             ) : Boolean {
                 val fromPosition = from.adapterPosition
                 val toPosition = to.adapterPosition
+                if (fromPosition < toPosition) {
+                    for (i in fromPosition until toPosition) {
+                        Collections.swap(userList, i, i+1)
+                        val fromIdx = userList[i].idx
+                        val toIdx = userList[i+1].idx
+                        userList[i].idx = toIdx
+                        userList[i+1].idx = fromIdx
+                    }
+                } else {
+                    for (i in fromPosition downTo toPosition+1){
+                        Collections.swap(userList, i, i-1)
+                        val fromIdx = userList[i].idx
+                        val toIdx = userList[i-1].idx
+                        userList[i].idx = toIdx
+                        userList[i-1].idx = fromIdx
+                    }
+                }
                 adapter.notifyItemMoved(fromPosition, toPosition)
-                viewModel.changeIdx(fromPosition, toPosition)
                 return false
             }
 
-            override fun onSwiped(viewHolder : ViewHolder, swipeDir : Int) {
+            override fun onSwiped(viewHolder: ViewHolder, swipeDir: Int) {
                 val swipedPosition = viewHolder.adapterPosition
                 viewModel.delete(swipedPosition)
                 adapter.notifyItemRemoved(swipedPosition)
