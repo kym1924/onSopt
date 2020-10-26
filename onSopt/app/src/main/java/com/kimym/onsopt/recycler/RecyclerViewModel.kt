1384871b7df9c46a5d6f83a94a155d0461c49fd7
kym1924 <kym1924@naver.com>
2020-10-27 00:34:06 +0900

    [UPDATE] RecyclerViewModel.changeLayout()

--- onSopt/app/src/main/java/com/kimym/onsopt/recycler/RecyclerViewModel.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/recycler/RecyclerViewModel.kt
@@ -1,8 +1,10 @@
 package com.kimym.onsopt.recycler
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kimym.onsopt.R
 import com.kimym.onsopt.room.User
 import com.kimym.onsopt.room.UserDao
 import kotlinx.coroutines.Dispatchers
@@ -12,6 +14,10 @@ class RecyclerViewModel : ViewModel(){
 
     lateinit var userDao : UserDao
     lateinit var allUsers : LiveData<List<User>>
+
+    private val _layoutItem = MutableLiveData<Int>(R.layout.item_recycler_linear)
+    val layoutItem : LiveData<Int>
+        get() = _layoutItem
 
     fun init(dao : UserDao) {
         userDao = dao
@@ -29,5 +35,10 @@ class RecyclerViewModel : ViewModel(){
     fun resetList(userList : MutableList<User>, users : List<User>) {
         userList.clear()
         userList.addAll(users)
+    }
+
+    fun changeLayout(){
+        if(_layoutItem.value==R.layout.item_recycler_linear) _layoutItem.value = R.layout.item_recycler_grid
+        else _layoutItem.value = R.layout.item_recycler_linear
     }
 }
