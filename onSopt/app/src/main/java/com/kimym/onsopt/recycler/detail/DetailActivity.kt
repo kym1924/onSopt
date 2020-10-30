cb9cb67db35765047e837c329c9fe969f6cecfa8
kym1924 <kym1924@naver.com>
2020-10-30 15:11:55 +0900

    [MODIFY] DetailActivity data binding

--- onSopt/app/src/main/java/com/kimym/onsopt/recycler/detail/DetailActivity.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/recycler/detail/DetailActivity.kt
@@ -1,30 +1,17 @@
 package com.kimym.onsopt.recycler.detail
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.kimym.onsopt.R
 import com.kimym.onsopt.databinding.ActivityDetailBinding
-import com.kimym.onsopt.room.User
 
 class DetailActivity : AppCompatActivity() {
-
-    private val detailViewModel : DetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding : ActivityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
-        binding.detailViewModel = detailViewModel
-
-    }
-
-    override fun onStart(){
-        super.onStart()
-
-        val user = intent.getParcelableExtra<User>("user")
-        detailViewModel.init(user!!)
-
+        binding.user = intent.getParcelableExtra("user")
     }
 }
