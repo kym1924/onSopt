6afffbdb05e6081a85e13660f60b13202b494181
kym1924 <kym1924@naver.com>
2020-11-09 18:16:31 +0900

    [UPDATE] BindingAdapter ifActive

--- onSopt/app/src/main/java/com/kimym/onsopt/ui/signup/SignUpBinding.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/ui/signup/SignUpBinding.kt
@@ -2,11 +2,21 @@ package com.kimym.onsopt.ui.signup
 
 import android.view.View
 import androidx.databinding.BindingAdapter
+import com.kimym.onsopt.R
 
 object SignUpBinding {
     @BindingAdapter("passwordCheck")
     @JvmStatic
     fun passwordCheck(view: View, isSamePassword: Boolean) {
         view.visibility = if (isSamePassword) View.VISIBLE else View.INVISIBLE
+    }
+
+    @BindingAdapter("ifActive")
+    @JvmStatic
+    fun ifActive(view : View, content : String) {
+        when (content) {
+            "" -> view.setBackgroundResource(R.drawable.background_edit_text)
+            else -> view.setBackgroundResource(R.drawable.background_edit_text_active)
+        }
     }
 }
