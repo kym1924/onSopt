e48d36e48799602e8657a7e5d3da1b22245f49dd
kym1924 <kym1924@naver.com>
2020-11-21 21:05:07 +0900

    [UPDATE] set animation for tv_info & tv_other in Fragments

--- onSopt/app/src/main/java/com/kimym/onsopt/ui/home/InfoFragment.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/ui/home/InfoFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kimym.onsopt.R
+import kotlinx.android.synthetic.main.fragment_info.*
 
 class InfoFragment : Fragment() {
 
@@ -14,5 +15,18 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        tv_info.scaleX = 0f
+        tv_info.scaleY = 0f
+        tv_info.animate().apply {
+            duration=2000
+            scaleX(1f)
+            scaleY(1f)
+        }.start()
+
     }
 }
--- onSopt/app/src/main/java/com/kimym/onsopt/ui/home/OtherFragment.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/ui/home/OtherFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kimym.onsopt.R
+import kotlinx.android.synthetic.main.fragment_other.*
 
 class OtherFragment : Fragment() {
     
@@ -14,5 +15,17 @@ class OtherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_other, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        tv_other.scaleX = 0f
+        tv_other.scaleY = 0f
+        tv_other.animate().apply {
+            duration=2000
+            scaleX(1f)
+            scaleY(1f)
+        }.start()
     }
 }
