d8ac67f7641dc1d9d275e3fc14050d6f193ab6f8
kym1924 <kym1924@naver.com>
2020-11-27 09:51:01 +0900

    [UPDATE] BindingAdapter setDateText

--- onSopt/app/src/main/java/com/kimym/onsopt/ui/search/SearchBinding.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/ui/search/SearchBinding.kt
@@ -1,6 +1,7 @@
 package com.kimym.onsopt.ui.search
 
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.kimym.onsopt.data.model.Document
@@ -16,5 +17,11 @@ object SearchBinding {
     @JvmStatic
     fun setListItem(recyclerView : RecyclerView, webs : List<Document>?) {
         if (recyclerView.adapter != null) with(recyclerView.adapter as SearchAdapter) { webs?.let{ setResults(it) } }
+    }
+
+    @BindingAdapter("setDateText")
+    @JvmStatic
+    fun setDateText(textView : TextView, dateString : String) {
+        textView.text = dateString.split("T")[0]
     }
 }
