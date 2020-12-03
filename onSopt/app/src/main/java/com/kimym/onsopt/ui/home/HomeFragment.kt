8bc11378274b5b2cf363ecc444f83099bed06868
kym1924 <kym1924@naver.com>
2020-12-03 19:30:54 +0900

    [REFACTOR] HomeFragment

--- onSopt/app/src/main/java/com/kimym/onsopt/ui/home/HomeFragment.kt
+++ onSopt/app/src/main/java/com/kimym/onsopt/ui/home/HomeFragment.kt
@@ -6,25 +6,56 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.kimym.onsopt.R
 import com.kimym.onsopt.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private val homeViewModel : HomeViewModel by activityViewModels()
-    lateinit var binding : FragmentHomeBinding
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
-        binding.homeViewModel = homeViewModel
-        binding.fragmentManager = childFragmentManager
-        binding.viewPager = binding.vpHome
-        binding.bindingTabLayout = binding.tabLayout
+        val binding : FragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         binding.lifecycleOwner = this@HomeFragment
+
+        setTabLayoutListener(binding)
+        setViewPager(binding)
+
         return binding.root
+    }
+
+    private fun setViewPager(binding : FragmentHomeBinding) {
+        binding.vpHome.adapter = HomePagerAdapter(childFragmentManager)
+
+        binding.vpHome.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                binding.tabLayout.getTabAt(position)!!.select()
+            }
+        })
+    }
+
+    private fun setTabLayoutListener(binding : FragmentHomeBinding) {
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("INFO"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("OTHER"))
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabSelected(tab: TabLayout.Tab?) { binding.vpHome.currentItem = tab!!.position }
+        })
     }
 }
